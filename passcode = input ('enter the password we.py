4e209536ee93b4e9fa178a6b2331a640487d5510
clipzzzz <136724391+clipzzzz@users.noreply.github.com>
2023-09-02 01:39:47 +0300

    Create passcode = input ('enter the password we.py

---
+++ passcode = input ('enter the password we.py
@@ -0,0 +1,30 @@
+passcode = input ('enter the password we gave you after the purchase')
+if (passcode) == ('passwordrdrdrd'):
+  chooseoption = input ('choose what you want to see ')
+  if (chooseoption) == ('nitr0 gen'):
+   print ('level 1 : http://sitesgithub.github.io/nitro/index0.html')
+   print ('level 2 : https://replit.com/@jacobnassar/Nitro-Generator-Checker')
+  elif (chooseoption) == ('spoofer'):
+    print ('not ready yet, tho you earned 0,50$ for finding that')
+  elif (chooseoption) == ('tip'):
+    print ('our paypal is exotic.hub.shop@gmail.com, send anything you want')
+  elif (chooseoption) == ('list'):
+    print ('able options are: nitr0 gen, tip, list, menu, nuker')
+  elif (chooseoption) == ('menu'):
+    menuselect = input ('dll or 3rd app??')
+    if (menuselect) == ('dll'):
+      print ('https://cdn.discordapp.com/attachments/1135618970905485372/1144636453045944370/d3d10.dll')
+    elif (menuselect) == ('3rd app'):
+      print ('https://cdn.discordapp.com/attachments/1127236871571910746/1143174297993621626/sadsa_1.rar')
+    elif (menuselect) != ('3rd app', 'dll'):
+      print ('your word doesnt match a selection')
+    else :
+      print ('error try again')
+  elif (chooseoption) == ('nuker'):
+    print ('https://cdn.discordapp.com/attachments/1127236871571910746/1147294916679061694/LithiumNukerV2.rar')
+  else :
+    print ('error, try again')
+elif (passcode) != ('passwordrdrdrd'):
+  print ('nah nigga, stop trying')
+else :
+  print ('error, try again!')
